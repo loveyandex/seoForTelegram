bc157e3637ff9b9d181d2f2a0c79218f2368557e
abvaalgit <abfzlabfzl@gmail.com>
2019-03-01 02:56:49 +0330

    cdfd

--- src/main/java/com/example/ResApi.java
+++ src/main/java/com/example/ResApi.java
@@ -1,10 +1,14 @@
 package com.example;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -12,21 +16,39 @@ public class ResApi {
 
 
     @GetMapping("/div")
-    public String D(){
+    public String D() {
         return "king";
     }
 
 
     @RequestMapping(value = "/send/{text}"
             , method = RequestMethod.GET)
-    public void gett(@PathVariable String text, HttpServletRequest request,
-                     HttpServletResponse response) {
+    public String gett(@PathVariable String text, HttpServletRequest request,
+                     HttpServletResponse response) throws IOException {
         String token = "bot700687388:AAHGmzovGb0LVXKRZAkechWrHBst7BJPMjw";
 
-        response.setHeader("Location", "https://api.telegram.org/"
+        String url = "https://api.telegram.org/"
                 + token
                 + "/sendMessage?chat_id=145464749&text="
-                + text);
-        response.setStatus(302);
+                + text;
+
+        return run(url);
+
+//        response.setHeader("Location", url);
+//        response.setStatus(302);
     }
+
+
+    String run(String url) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
 }
