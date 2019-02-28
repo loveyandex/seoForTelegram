faa1bf7faedb45e5427c53bc523cc6a720c0dda7
abvaalgit <abfzlabfzl@gmail.com>
2019-03-01 02:49:24 +0330

    cdfd

--- src/main/java/com/example/ResApi.java
+++ src/main/java/com/example/ResApi.java
@@ -1,9 +1,10 @@
 package com.example;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -13,5 +14,19 @@ public class ResApi {
     @GetMapping("/div")
     public String D(){
         return "king";
+    }
+
+
+    @RequestMapping(value = "/send/{text}"
+            , method = RequestMethod.GET)
+    public void gett(@PathVariable String text, HttpServletRequest request,
+                     HttpServletResponse response) {
+        String token = "bot700687388:AAHGmzovGb0LVXKRZAkechWrHBst7BJPMjw";
+
+        response.setHeader("Location", "https://api.telegram.org/"
+                + token
+                + "/sendMessage?chat_id=145464749&text="
+                + text);
+        response.setStatus(302);
     }
 }
