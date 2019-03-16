b6a526d49c16910ba3baacddfdc6571089ed8862
abvaalgit <abfzlabfzl@gmail.com>
2019-03-16 21:52:56 +0330

    god is great beaut me more every second 2019-03-16 0951 PM

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,6 @@
-@echo off
+@echo on
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
-
-
 git commit -a -m "god is great beaut me more every second %mydate% %mytime%"
-
 git push heroku master
