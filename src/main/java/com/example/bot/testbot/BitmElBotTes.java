30015829a76aa3f1eb19dd5cabb8a18c582f95cf
aminabvaal <ag1992@aut.ac.ir>
2019-04-20 21:34:03 +0430

    god is great beaut me more every second 2019-04-20 0934 PM

--- src/main/java/com/example/bot/testbot/BitmElBotTes.java
+++ src/main/java/com/example/bot/testbot/BitmElBotTes.java
@@ -1,16 +1,13 @@
 package com.example.bot.testbot;
 
 import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
-import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.*;
+import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
@@ -18,16 +15,17 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 
 /**
  * created By aMIN on 3/23/2019 4:37 PM
  */
-
+@Component
 public class BitmElBotTes extends TelegramLongPollingBot {
 
     public static void main(String[] args) {
-        
+
         ApiContextInitializer.init();
         TelegramBotsApi api = new TelegramBotsApi();
         try {
@@ -41,44 +39,56 @@ public class BitmElBotTes extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Long chatId = update.getMessage().getChatId();
 
-        if (false) {
 
-            SendPhoto sendPhoto = new SendPhoto();
-            sendPhoto.setChatId("@bitmel")
-                    .setPhoto("https://music96.ir/wp-content/uploads/2019/03/Shadmehr-Aghili-Vaghti-Ke-Bad-Misham1-1000x1000.jpg ")
-                    .setCaption("#shadmehr\n" +
-                            "https://t.me/bitmelbot?start=Dshadmehr")
-            ;
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String text = message.getText();
+            }
+            if (message.hasPhoto()) {
+                List<PhotoSize> photo = message.getPhoto();
+                try {
+                    SendPhoto sendPhoto = new SendPhoto();
+                    sendPhoto.setChatId(chatId)
+                            .setPhoto(photo.get(0).getFileId())
+                            .setCaption(message.getCaption())
+                    ;
+                    execute(sendPhoto);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (message.hasDocument()) {
+                Document document = message.getDocument();
 
-            try {
-                Message message = execute(sendPhoto);
-                System.err.println(new Gson().toJson(message));
-                SendAudio sendAudio = new SendAudio()
-                        .setChatId("@bitmel")
-                        .setAudio("https://t.me/musicaminbot/86")
-                        .setAudio("CQADBAADBwYAAuhGYVDYG9SW_FYZtQI")
-                        .setAudio("https://t.me/awherestoragesongs/213")
-                        .setAudio("https://t.me/bitmel/7")
-                        .setReplyToMessageId(message.getMessageId());
+            }
+            if (message.hasVideo()) {
+                Video video = message.getVideo();
 
-                execute(sendAudio);
+            }
+            if (message.hasVideoNote())
+            {
+                VideoNote videoNote = message.getVideoNote();
+                SendVideoNote sendVideoNote = new SendVideoNote();
+                sendVideoNote.setChatId(chatId)
+                        .setVideoNote(videoNote.getFileId());
+                try {
+                    execute(sendVideoNote);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
 
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
             }
 
 
-        } else {
 
-            try {
-                execute(new SendMessage(update.getMessage().getChatId(), update.getMessage().getText()));
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+
+
         }
-
     }
+
 
     @Override
     public String getBotUsername() {
