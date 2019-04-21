cca060208e2ec556248f1350c907f7f70b7d20d3
aminabvaal <ag1992@aut.ac.ir>
2019-04-22 02:09:04 +0430

    god is great beaut me more every second 2019-04-22 0209 AM

---
+++ src/main/java/com/example/bot/chat-ripia/testbot/BitmElBotTes.java
@@ -0,0 +1,90 @@
+package com.example.bot.testbot;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.*;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+
+/**
+ * created By aMIN on 3/23/2019 4:37 PM
+ */
+@Component
+public class BitmElBotTes extends TelegramLongPollingBot {
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Long chatId = update.getMessage().getChatId();
+
+
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String text = message.getText();
+            }
+            if (message.hasPhoto()) {
+                List<PhotoSize> photo = message.getPhoto();
+                try {
+                    SendPhoto sendPhoto = new SendPhoto();
+                    sendPhoto.setChatId(chatId)
+                            .setPhoto(photo.get(0).getFileId())
+                            .setCaption(message.getCaption())
+                    ;
+                    execute(sendPhoto);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (message.hasDocument()) {
+                Document document = message.getDocument();
+
+            }
+            if (message.hasVideo()) {
+                Video video = message.getVideo();
+
+            }
+            if (message.hasVideoNote())
+            {
+                VideoNote videoNote = message.getVideoNote();
+                SendVideoNote sendVideoNote = new SendVideoNote();
+                sendVideoNote.setChatId(chatId)
+                        .setVideoNote(videoNote.getFileId());
+                try {
+                    execute(sendVideoNote);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+
+
+
+        }
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return "858001251:AAHN2aSt00ttRzUOsXX4_Mq5jKugLBI8ndQ";
+    }
+}
---
+++ src/main/java/com/example/bot/chatripia/ChatRipiaBot.java
@@ -0,0 +1,112 @@
+package com.example.bot.chatripia;
+
+import com.example.Meths;
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendVideoNote;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+/**
+ * created By aMIN on 3/23/2019 4:37 PM
+ */
+@Component
+public class ChatRipiaBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Long chatId = update.getMessage().getChatId();
+            Message message = update.getMessage();
+            User from = message.getFrom();
+            ResultSet resultSet2 = null;
+            try {
+                resultSet2 = connection.createStatement().executeQuery("select * from Muser;");
+                while (resultSet2.next()) {
+                    String idofsend = String.valueOf(resultSet2.getInt(1));
+                    execute(new SendMessage(idofsend
+                            , message.getText()));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (TelegramApiException e) {
+                try {
+                    execute(new SendMessage(chatId
+                            , message.getText()));
+                } catch (TelegramApiException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            try {
+                execute(new SendMessage(chatId
+                        , message.getText()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Autowired
+    private Connection connection;
+
+    @Bean
+    public void dbs() {
+        try {
+            connection.createStatement().execute("create table if not exists ChatUser(id serial primary key,name varchar(30) )");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private boolean addUser(int idj) {
+        String sql = "INSERT INTO chatuser (id)" +
+                "    SELECT ?" +
+                "WHERE NOT EXISTS (" +
+                "    SELECT id FROM Muser WHERE id=? " +
+                ");";
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, idj);
+            preparedStatement.setInt(2, idj);
+            preparedStatement.setInt(2, idj);
+            boolean execute = preparedStatement.execute();
+            return execute;
+
+        } catch (SQLException e) {
+        }
+        return (true);
+
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return "858288629:AAEpT32pWgpkTHuWlwZ-42ugnCWlXoBmKNM";
+    }
+}
