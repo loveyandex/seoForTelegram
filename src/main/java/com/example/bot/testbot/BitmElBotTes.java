35253b6c609c82dfa8cf9466308c05783b4cf46e
aminabvaal <ag1992@aut.ac.ir>
2019-04-22 02:14:04 +0430

    god is great beaut me more every second 2019-04-22 0214 AM

--- src/main/java/com/example/bot/testbot/BitmElBotTes.java
+++ src/main/java/com/example/bot/testbot/BitmElBotTes.java
@@ -24,7 +24,6 @@ import java.util.List;
 @Component
 public class BitmElBotTes extends TelegramLongPollingBot {
 
-
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
